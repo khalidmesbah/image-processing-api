46c4a1df0fc080ab395331d1bb1db1c775442379
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-02-23 16:58:17 +0200

    add new endpoints

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -12,18 +12,22 @@ const router = express.Router();
 
 // endpoint for resizing the image
 router.get("/resize", async (req: Request, res: Response): Promise<void> => {
-  const image = (req.query.image as unknown as string).slice(0, -4);
+  let image = req.query.image as unknown as string;
   const width = Math.abs(parseInt(req.query.width as string));
   const height = Math.abs(parseInt(req.query.height as string));
-
   // make sure the parameters values are correct otherwise redirect the error page
   if (
-    !availableImages.includes(image.concat(".jpg")) ||
-    width <= 0 ||
-    height <= 0
+    typeof req.query.width === `undefined` ||
+    typeof req.query.height === `undefined` ||
+    typeof req.query.image === `undefined`
   ) {
-    res.redirect(path.join(__dirname, `/views`, `404.js`));
+    res.send(`Missing filename, height or width.`);
+  } else if (!availableImages.includes(image)) {
+    res.send(`Invalid input for imagename`);
+  } else if (width <= 0 || height <= 0) {
+    res.send(`Invalid input for height or width`);
   } else {
+    image = image.slice(0, -4);
     if (
       fs.existsSync(
         path.join(
@@ -51,7 +55,7 @@ router.get("/resize", async (req: Request, res: Response): Promise<void> => {
           );
         });
       } catch (error) {
-        res.redirect(path.join(__dirname, `/views`, `404.js`));
+        res.send(`couldn't resize the image`);
       }
     }
   }
@@ -72,15 +76,17 @@ router.get("/images", async (req: Request, res: Response): Promise<void> => {
 });
 
 // endpoint for displaying a specific image
-router.get(
-  "/image/?:id",
-  async (req: Request, res: Response): Promise<void> => {
+router
+  .get("/image", async (req: Request, res: Response): Promise<void> => {
+    res.send(`Type the id of the image`);
+  })
+  .get("/image/?:id", async (req: Request, res: Response): Promise<void> => {
     const id: number = parseInt(req.params.id);
     if (1 <= id && id <= 5) {
       res.send(availableImages[id - 1]);
     } else {
       res.send(`image with id = ${id} is not found`);
     }
-  }
-);
+  });
+
 export default router;
